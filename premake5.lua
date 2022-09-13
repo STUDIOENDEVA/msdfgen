747314e7b551734f3cd127407d4b9ce7c5f50922
endevagames <samuel.mungy@endevagames.com>
2022-09-13 21:52:45 +0300

    Setup premake

---
+++ premake5.lua
@@ -0,0 +1,59 @@
+include "freetype"
+
+project "msdfgen"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	location "%{wks.location}/ProjectFiles"	
+	targetdir ( bindir .. "/%{prj.name}")
+	objdir ( intdir .. "/%{prj.name}")
+	
+	files
+	{
+		"core/**.h",
+		"core/**.hpp",
+		"core/**.cpp",
+		"ext/**.h",
+		"ext/**.hpp",
+		"ext/**.cpp",
+		"lib/**.cpp",
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include",
+		"freetype/include"
+	}
+
+	defines
+	{
+		"MSDFGEN_USE_CPP11"
+	}
+
+	links
+	{
+		"freetype"
+	}
+
+	filter "configurations:Debug"
+            runtime "Debug"
+	    symbols "on"
+
+	filter "configurations:DebugEditor"
+            runtime "Debug"
+	    symbols "on"
+
+	filter "configurations:Release"
+            runtime "Release"
+	    optimize "on"
+
+	filter "configurations:ReleaseEditor"
+            runtime "Release"
+	    optimize "on"
+
+	filter "configurations:Dist"
+            runtime "Release"
+	    optimize "on"
